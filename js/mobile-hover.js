d443d88dab8c00a492155606e9adcfd1ba09439f
JakubRalis <ralisjakub05@gmail.com>
2023-04-04 01:49:23 +0200

    mobile hover

--- js/mobile-hover.js
+++ js/mobile-hover.js
@@ -1,5 +1,12 @@
-let product = document.getElementById("product");
-
-function touch() {
-    product.classList.toggle("touched")
+function touch1() {
+    document.getElementById("product1").classList.toggle("touched")
+}
+function touch2() {
+    document.getElementById("product2").classList.toggle("touched")
+}
+function touch3() {
+    document.getElementById("product3").classList.toggle("touched")
+}
+function touch4() {
+    document.getElementById("product4").classList.toggle("touched")
 }
