0118ec0015b9d93442082f4a57f4e00ff9b1a3ea
JakubRalis <ralisjakub05@gmail.com>
2023-04-05 22:56:55 +0200

    hoodies script

--- js/mobile-hover.js
+++ js/mobile-hover.js
@@ -1,12 +1,4 @@
-function touch1() {
-    document.getElementById("product1").classList.toggle("touched")
-}
-function touch2() {
-    document.getElementById("product2").classList.toggle("touched")
-}
-function touch3() {
-    document.getElementById("product3").classList.toggle("touched")
-}
-function touch4() {
-    document.getElementById("product4").classList.toggle("touched")
+function touch(e) {
+    console.log(e);
+    e.classList.toggle("touched")
 }
