cf1884a9ab399d09fb591f15515507ecb11869c1
JakubRalis <ralisjakub05@gmail.com>
2023-04-06 11:44:32 +0200

    mobile-hover fix

--- js/mobile-hover.js
+++ js/mobile-hover.js
@@ -2,15 +2,15 @@ let products = document.getElementsByClassName("products-box-link");
 
 function touch(e) {
     console.log(e);
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].classList.contains("touched") && products[i] != e) {
+            products[i].classList.remove("touched");
+        }
+    }
     if (e.classList.contains("touched")) {
         return;
     }
     else {
-        for (let i = 0; i < products.length; i++) {
-            if (products[i].classList.contains("touched")) {
-                products[i].classList.remove("touched");
-            }
-        }
         e.classList.add("touched");
     }
 }
