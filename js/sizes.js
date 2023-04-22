85af0981b26ec1c03d7502922607f797311007ef
JirkaX <osmargarov@gmail.com>
2023-04-22 11:59:41 +0200

    maly skodny

--- js/sizes.js
+++ js/sizes.js
@@ -1,16 +1,19 @@
-let buttons = document.querySelectorAll(".sizes-item");
-
-function active(e) {
-    console.log(e);
-    if (e.classList.contains("active")) {
-        e.classList.remove("active");
-    }
-    else {
-        for (let i = 0; i < buttons.length; i++) {
-            if (buttons[i].classList.contains("active")) {
-                buttons[i].classList.remove("active");
+document.addEventListener("DOMContentLoaded", (evt) => {
+    const buttons = document.querySelectorAll(".available");
+    for (const button of buttons) {
+        button.addEventListener("click", (e) => {
+            console.log(e.currentTarget);
+            if (e.currentTarget.classList.contains("active")) {
+                e.currentTarget.classList.remove("active");
             }
-        }
-        e.classList.add("active");
+            else {
+                for (let i = 0; i < buttons.length; i++) {
+                    if (buttons[i].classList.contains("active")) {
+                        buttons[i].classList.remove("active");
+                    }
+                }
+                e.currentTarget.classList.add("active");
+            }
+        })
     }
-}
+})
