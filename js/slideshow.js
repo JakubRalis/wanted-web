8e021fd512cbfd1adb51fa235baad3a3cf90cc1d
JakubRalis <ralisjakub05@gmail.com>
2023-05-24 18:23:38 +0200

    ssss

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,81 +1,17 @@
-  /*  
-document.addEventListener("DOMContentLoaded", (e) => {
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll(".main-image")
+    const first = slides[0]
+    const second = slides[1]
+    const third = slides[2]
 
-    $('#slider').on('touchstart', function(event) {
-        const xClick = event.originalEvent.touches[0].pageX;
-      
-        $(this).on('touchmove', function(event) {
-          const xMove = event.originalEvent.touches[0].pageX;
-          const sensitivityInPx = 5;
-      
-          if (Math.floor(xClick - xMove) > sensitivityInPx) {
-            plusSlides(1);
-            $(this).off('touchmove');
-          } 
-          else if (Math.floor(xClick - xMove) < -sensitivityInPx) {
-            plusSlides(-1);
-            $(this).off('touchmove');
-          }
-        });
-      });
-      
-      const slides = document.querySelectorAll(".slide");
-      const dots = document.querySelectorAll(".dot");
-
-      let slideIndex = 0;
-      const maxIndex = slides.length - 1
-      let next = slideIndex + 1
-      let previous = maxIndex
-      
-      console.log(previous)
-      console.log(slideIndex)
-      console.log(next)
-
-      showSlides(slideIndex);
-      
-      function plusSlides(n) {
-        slideIndex += n
-        next += n
-        previous += n
-        showSlides(n);
-      }
-      
-      function showSlides(n) {
-        if (slideIndex > maxIndex) {
-          slideIndex = 0
-        }
-        else if (slideIndex < 0) {
-          slideIndex = maxIndex
-        }
-        if (next > maxIndex) {
-          next = 0
-        }
-        else if (next < 0) {
-          next = maxIndex
-        }
-        if (previous > maxIndex) {
-          previous = 0
-        }
-        else if (previous < 0) {
-          previous = maxIndex
-        }
-
-        for (let i = 0; i < slides.length; i++) {
-          slides[slideIndex].classList.add("current");
-          slides[next].classList.add("next")
-          slides[previous].classList.add("previous")
-          if (n < 0) {
-            slides[next].classList.add("reverse")
-          }
-          if (slides[i].classList.contains("current") && i != slideIndex) {
-            slides[i].classList.remove("current")
-          }
-          else if (slides[i].classList.contains("next") && i != next) {
-            slides[i].classList.remove("next")
-          }
-          else if (slides[i].classList.contains("previous") && i != previous) {
-            slides[i].classList.remove("previous")
-          }
-        }
-      })
-        */ 
+    if (window.innerWidth <= 1245 && window.innerWidth > 750) {
+        first.setAttribute("src", "./media/skater-1245px.jpg")
+        second.setAttribute("src", "./media/skater2-1245px.jpg")
+        third.setAttribute("src", "./media/skater3-1245px.jpg")
+    }
+    else if (window.innerWidth <= 750) {
+        first.setAttribute("src", "./media/skater-750px.jpg")
+        second.setAttribute("src", "./media/skater2-750px.jpg")
+        third.setAttribute("src", "./media/skater3-750px.jpg")
+    }
+})
